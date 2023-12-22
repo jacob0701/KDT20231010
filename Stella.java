c3ec8a37fa532f3b9366260d55e63613578e36ff
jacob0701 <142728227+jacob0701@users.noreply.github.com>
2023-12-22 23:40:10 +0900

    Add files via upload

---
+++ Stella.java
@@ -0,0 +1,105 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class Stella {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt();
+//		
+//		
+//		for (int i = 1; i <= N; i++) {
+//			
+//			for (int j = N+30; j >= i; j--) {   // 임의 값을 더하여 왼쪽 공간을 띄
+//				
+//				System.out.print(" ");
+//												// 공백으로 역직각삼각형 
+//			} 
+//			
+//			for (int k = 1; k <= 2*i-1; k++) {
+//				
+//				System.out.print("*");
+//				
+//			}
+//			      								// 정삼각형 
+//			System.out.println();
+//			
+//		} 
+//		
+//				
+//				// 별의 상 완성 
+//		
+//		
+//		
+//		
+//		for (int i = 2*N; i >= 1; i--) {
+//			
+//			if (i == N) break;					
+//											// 열의 수를 절반에서 멈추게 
+//		
+//			for (int j = N+30; j >= i; j--) {
+//		
+//			System.out.print(" ");
+//											// 공백으로 역직각삼각형  
+//			} 
+//		
+//			for (int k = 1; k <= i*2-1; k++) {
+//				
+//				System.out.print("*");
+//											// 역정삼각형의 절반 
+//			} System.out.println();
+//		
+//		}
+//		
+//		
+//		
+//				// 별의 중단 부분 완성 
+//		
+//		
+//		
+//		
+//		
+//		for (int i = 1; i <= N; i++) {
+//			
+//			for (int j = 1; j <= N+30-N; j++) {
+//				
+//				System.out.print(" ");
+//												// 왼쪽 직각삼각형 여백 
+//			}
+//			
+//			
+//			for (int k = N; k >= i; k--) {
+//				
+//				System.out.print("*");
+//				
+//			}	
+//												// 왼쪽 다리부분 
+//			
+//			for (int m = 1; m <= 2*i-1; m++) {
+//				
+//				System.out.print(" ");
+//				
+//			}
+//												// 공백으 정삼각형 
+//			for (int k = N; k >= i; k--) {
+//				
+//				System.out.print("*");
+//				
+//			}	
+//												// 오른 다리부분 			
+//		
+//		
+//				// 별의 하단 완성 
+//		
+//			
+//			System.out.println();
+//			
+//		}
+		
+		
+	}
+}
---
+++ diamond.java
@@ -0,0 +1,55 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class diamond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int N = sc.nextInt(); 
+//		
+//		
+//		for (int i = 1; i <= N-1; i++) {
+//			
+//			for (int j = N; j >= i; j--) {
+//				
+//				System.out.print(" ");
+//												// 공백으로 만든 역직각삼각형 
+//			}
+//			
+//			for (int k = 1; k <= i*2-1; k++) {
+//				
+//				System.out.print("*");
+//				
+//			} 
+//												// 정삼각형 (마름모의 상단)
+//			System.out.println();
+//			
+//		}
+//		
+//		
+//		for (int i = N; i >= 1; i--) {
+//			
+//			for (int j = N; j >= i; j--) {
+//			
+//				System.out.print(" ");
+//												// 공백으로 만든 직각삼각형 
+//			} 
+//			
+//			for (int k = 1; k <= i*2-1; k++) {
+//				
+//				System.out.print("*");
+//												// 역정삼각형 (마름모의 하단)
+//			}
+//
+//			System.out.println();
+//			
+//		}	
+		
+	}
+
+}
