b2b57be29ecf8d0fbedb714b8322b7b1855593bc
jcob-yim <142728227+jcob-yim@users.noreply.github.com>
2024-01-25 23:59:37 +0900

    Add files via upload

---
+++ ArrayPractice2.java
@@ -0,0 +1,16 @@
+package array;
+
+public class ArrayPractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] menu = {11,12,13,14,15};
+		
+		for (int i = 0; i < 5; i++) {
+//			menu[i] = i +1 ;
+			System.out.println(menu);
+		}
+		
+	}
+
+}
